be780e29514205298a5ef7d31f64e80658641455
rinku saini <80007809d@gmail.com>
2023-06-25 23:16:41 +0530

    first commit

---
+++ Conditional.java
@@ -0,0 +1,18 @@
+
+
+public class Conditional {
+    public static void main (String []args)
+    {
+        int age=35;
+        if(age>=18){
+            System.out.println("adult");
+
+        }
+        else{
+            System.out.println("not adult");
+        }
+        System.out.println("rinku");
+
+    }
+    
+}
